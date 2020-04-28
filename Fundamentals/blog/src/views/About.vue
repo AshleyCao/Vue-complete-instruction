f42996f385a608d8a76fe032dfb075f195611ad7
Ashley Cao <acao@zendesk.com>
2020-04-28 22:31:44 +1000

    bsVue running

--- Fundamentals/blog/src/views/About.vue
+++ Fundamentals/blog/src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is a result page</h1>
+    <b-card class="mt-3" header="Form Data Result">
+      <pre class="m-0">{{ form }}</pre>
+    </b-card>
   </div>
 </template>
